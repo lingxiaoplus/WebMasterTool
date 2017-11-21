36cca99a3d99c49834fecd4d8b3a4bc332b6a4a0
lingxiaopua <1083514716@qq.com>
2017-11-21 20:48:07 +0800

    seo初步完成

--- app/src/main/java/com/lingxiao/webmastertool/seo/SeoInfoActivity.java
+++ app/src/main/java/com/lingxiao/webmastertool/seo/SeoInfoActivity.java
@@ -1,6 +1,7 @@
 package com.lingxiao.webmastertool.seo;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -8,7 +9,6 @@ import android.widget.Toast;
 import com.lingxiao.webmastertool.BaseActivity;
 import com.lingxiao.webmastertool.R;
 import com.lingxiao.webmastertool.globle.ContentValue;
-import com.lingxiao.webmastertool.utils.SnackUtils;
 import com.lingxiao.webmastertool.utils.SpUtils;
 import com.lingxiao.webmastertool.utils.UIUtils;
 
@@ -31,6 +31,12 @@ public class SeoInfoActivity extends BaseActivity {
     TextView seoKeywords;
     @BindView(R.id.seo_desc)
     TextView seoDesc;
+    @BindView(R.id.seo_swip)
+    SwipeRefreshLayout seoSwip;
+    @BindView(R.id.seo_age)
+    TextView seoAge;
+    @BindView(R.id.seo_server)
+    TextView seoServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,30 +52,42 @@ public class SeoInfoActivity extends BaseActivity {
         setSupportActionBar(toolbarTitle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        seoSwip.setColorSchemeColors(getResources().getColor(R.color.colorPrimary));
+        seoSwip.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getInfo();
+            }
+        });
     }
 
-    private void getInfo(){
+    private void getInfo() {
         final String url = ContentValue.SEOINFO +
                 SpUtils.getString(this,
-                        ContentValue.DOMAIN,"");
+                        ContentValue.DOMAIN, "");
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     Document document = Jsoup.connect(url).get();
-                    Elements elements = document.
-                            select("div.tcontent-seolist");
+                    final Elements elements = document.
+                            select("div.tcontent-seolist").select("p");
 
-                    final String s = elements.select("p").text();
+                    //final String s = elements.select("p").text();
                     UIUtils.runOnUIThread(new Runnable() {
                         @Override
                         public void run() {
-                            seoTitle.setText(s);
-                            Toast.makeText(getApplicationContext()
-                            ,s,Toast.LENGTH_LONG).show();
+                            seoTitle.setText(elements.get(0).text());
+                            seoAge.setText(elements.get(1).text());
+                            seoKeywords.setText(elements.get(3).text());
+                            seoDesc.setText(elements.get(4).text());
+                            seoServer.setText(elements.get(5).text());
+                            if (seoSwip.isRefreshing()) {
+                                seoSwip.setRefreshing(false);
+                            }
                         }
                     });
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
